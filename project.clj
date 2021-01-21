9352f3251c16b0ae88a6fb2c9afbb751b489a385
Wilker Lucio <wilkerlucio@gmail.com>
2021-01-20 21:13:43 -0300

    Add project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject com.wsscode/promesa-bridges "2021.01.20"
+  :description "Promesa integration with other async libraries."
+  :url "https://github.com/wilkerlucio/promesa-bridges"
+  :min-lein-version "2.7.0"
+  :license {:name "Eclipse Public License - v 2.0"
+            :url  "https://opensource.org/licenses/EPL-2.0"}
+  :dependencies [[funcool/promesa "6.0.0"]]
+
+  :source-paths ["src/main" "resources"]
+
+  :jar-exclusions [])
